7ff494c63336ea401e219e64f650253c801b9b56
Carlos Zepeda <carlos.ihernandez7@gmail.com>
2021-10-07 16:54:24 -0600

    end style

--- set_atados.py
+++ set_atados.py
@@ -69,14 +69,21 @@ class Doc_Reader:
         self.sheet = self.xls.parse(0)
         self.headers = [e for e in self.sheet]
 
+class Doc_Writer(Doc_Reader):
+    def __init__(self, file_name=None) -> None:
+        Doc_Reader.__init__(file_name)
+    def star_doc(self):
+        pass
+
 class Store_DB_Elements:
     def __init__(self) -> None:
-        self.laber_writer = LabelWriter("stick.html", default_stylesheets=("style.css",))
+        self.laber_writer = LabelWriter("stick.html", default_stylesheets=("style.css",), items_per_page=24)
         self.record = []
         self.product  = 0
         self.cuantity = 1
         self.price    = 2
-        self.nut      = 2
+        self.code     = 3
+        self.nut      = 4
 
         self.orders = {}
 
@@ -88,6 +95,7 @@ class Store_DB_Elements:
                 self.record.append(dict(order_num=order, client_name=self.orders[order], product_name=item_l[self.product], 
                                         product_cuantity=item_l[self.cuantity], product_price=item_l[self.price], product_nut=item_l[self.nut]))
         self.laber_writer.write_labels(self.record, target='test.pdf')
+        
     def show_order(self) -> dict:
         print("[Orden],     [Cliente],          [Producto],          [Cantidad],        [Precio],       [Tuerca]")
         for order in self.orders:
@@ -114,8 +122,6 @@ class Store_DB_Elements:
             del self.order_detail[order]
             del self.orders[order]
     
-    def find_pair(self) ->None:
-        pass
 
 class Run_Objs(Doc_Reader, Control_Server, Store_DB_Elements):
 
@@ -126,23 +132,25 @@ class Run_Objs(Doc_Reader, Control_Server, Store_DB_Elements):
         self.label_writer_f = 'Pedido_{0}_template.html'
         self.label_writer = None
         self.querys = {'insert_atado' : "INSERT INTO productosatados (idprimary, idsecundary) VALUES ({0},{1});",
-                       'get_cotizaci' : "SELECT CONCAT(IF(CT.idvendedor = 1, 'A', 'B'), {0}) AS N, CL.nombre AS Cliente, PR.descripcion AS Producto, DT.cantidad, DT.precio, PR.idproducto  FROM detallecotizacion AS DT INNER JOIN cotizaciones AS CT ON DT.idcotizacion = CT.idcotizacion INNER JOIN clientes AS CL ON CT.idcliente = CL.idcliente INNER JOIN documentos AS DOC ON CT.iddocumento = DOC.iddocumento INNER JOIN productos AS PR ON DT.idproducto = PR.idproducto WHERE CT.numero = {1};",
-                       'get_pair'     : "SELECT NULL AS Result FROM productosatados WHERE NOT EXISTS(SELECT idsecundary FROM productosatados WHERE idprimary = 899) UNION SELECT idsecundary FROM productosatados WHERE idprimary = 899;"
+                       'get_cotizaci' : "SELECT CONCAT(IF(CT.idvendedor = 1, 'A', 'B'), {0}) AS N, CL.nombre AS Cliente, PR.descripcion AS Producto, DT.cantidad, DT.precio, PR.idproducto, '---'  FROM detallecotizacion AS DT INNER JOIN cotizaciones AS CT ON DT.idcotizacion = CT.idcotizacion INNER JOIN clientes AS CL ON CT.idcliente = CL.idcliente INNER JOIN documentos AS DOC ON CT.iddocumento = DOC.iddocumento INNER JOIN productos AS PR ON DT.idproducto = PR.idproducto WHERE CT.numero = {1};",
+                       'get_pair'     : "SELECT NULL AS Result FROM productosatados WHERE NOT EXISTS(SELECT idsecundary FROM productosatados WHERE idprimary = {0}) UNION SELECT idsecundary FROM productosatados WHERE idprimary = {0};"
                        }
 
     def add_orders(self) -> None:
         l = []
         self.connect_server()
         while True:
-            order_n = input("Type Order Num")
-            cot_ = int(input("Type Correlative"))
+            order_n = input("Type Order Num: ")
             if order_n == '0':
                 self.show_order()
+                self.find_pair()
                 self.make_stiker()
+                return
+            cot_ = int(input("Type Correlative: "))
             data = self.get_statement(self.querys['get_cotizaci'].format(order_n, cot_))
             self.insert_order(data[0][0],data[0][1])
             for item in data:
-                l.append(list(item[2:5]))
+                l.append(list(item[2:7]))
             self.insert_order_detail(data[0][0], *l)
 
 
@@ -155,6 +163,17 @@ class Run_Objs(Doc_Reader, Control_Server, Store_DB_Elements):
         for item in self.list_:
             self.insert_statement(self.querys['insert_atado'], *item)
         self.disconnect_server(True)
+
+    def find_pair(self) ->None:
+        code_product_pair = None
+        for order in self.orders:
+            for n, order_d in enumerate(self.order_detail[order]):
+                code_product_pair = self.get_statement(self.querys['get_pair'].format(order_d[self.code]))[0][0]
+                if code_product_pair:
+                    if code_product_pair == self.order_detail[order][n+1][self.code] and order_d[self.cuantity] == self.order_detail[order][n+1][self.cuantity]:
+                        self.order_detail[order][n][self.price] = (self.order_detail[order][n][self.price] + self.order_detail[order][n+1][self.price])
+                        self.order_detail[order][n][self.nut] = 'Incluye Tuerca'
+                        del self.order_detail[order][n+1]
     
     def exec_app(self, option):
         self.open_doc()
@@ -163,6 +182,7 @@ class Run_Objs(Doc_Reader, Control_Server, Store_DB_Elements):
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         app = Run_Objs(sys.argv[1], connection_config2)
+        app.exec_app()
     else:
-        ap = Run_Objs('', connection_config)
+        ap = Run_Objs('', connection_config2)
         ap.add_orders()
