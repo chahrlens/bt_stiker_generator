258e5c2832831fd20358503fecf68b3817d70605
charls <charls@Latitude.DBethel>
2022-12-26 14:13:05 -0600

    load current directory or customized to save file

--- getSettings.py
+++ getSettings.py
@@ -1,9 +1,13 @@
 import configparser
+import os
 class SQLSettings:
     def __init__(self):
         self.file_name = "config.con"
-        self.parser = configparser.ConfigParser()
-        self.default = 'MYSQLCON'
+        self.parser          = configparser.ConfigParser()
+        self.default_path    = 'PATH'
+        self.dic_path        = {'path' : os.getcwd()}
+        self.path            = ''
+        self.default         = 'MYSQLCON'
         self.connection_conf = {
 	                            'host'    	 : 'localhost',
 	                            'database'	 : 'test',
@@ -17,13 +21,23 @@ class SQLSettings:
             self.get_file()
 
     def make_file(self):
-        self.parser[self.default] = self.connection_conf
+        #Save current path as default to Save File
+        self.parser[self.default_path]  = self.dic_path
+        #Save Connection Params as default
+        self.parser[self.default]       = self.connection_conf
         with open(self.file_name, 'w') as configfile:
             self.parser.write(configfile)
 
     
     def get_file(self):
+        #Read Config File
         self.parser.read(self.file_name)
+        
+        #Get Path to Save File
+        self.dic_path['path']               = self.parser[self.default_path]['path']
+        self.path                           = self.dic_path['path']
+
+        #Get Connection params
         self.connection_conf['host']        = self.parser[self.default]['host']
         self.connection_conf['database']    = self.parser[self.default]['database']
         self.connection_conf['user']        = self.parser[self.default]['user']
